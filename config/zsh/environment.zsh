e4474f0957f170d8586fe55dabaade5b62f0b4ab
Johan Friis <johanfriis@gmail.com>
2017-08-17 18:43:46 +0000

    add zsh environment file

---
+++ config/zsh/environment.zsh
@@ -0,0 +1,10 @@
+###################
+### ENVIRONMENT ###
+###################
+
+# redifine PATH completely
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/usr/local/sbin:/usr/local/bin:$PATH
+PATH=/usr/local/opt/zplug/bin:$PATH
+PATH=$HOME/bin:$PATH
+export PATH
