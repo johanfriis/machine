bf17d4bc5dea33786fdcd3aed99e940d8509f4bd
Johan Friis <johanfriis@gmail.com>
2017-08-18 21:21:37 +0000

    add zsh history keybinds

---
+++ config/zsh/keybinds.zsh
@@ -0,0 +1,12 @@
+################
+### KEYBINDS ###
+################
+
+# bind arrow up and down to history search
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
+
+# bind ctrl-p/n to histor search
+bindkey "" history-beginning-search-forward
+bindkey "" history-beginning-search-backward
+
