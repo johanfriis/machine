2ae05495dab1f1681aca7721bbe894ac6119246f
Johan Friis <johanfriis@gmail.com>
2017-11-07 14:20:56 +0100

    zsh - add some common commands and alias'

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,7 +1,13 @@
 #############
 ### ALIAS ###
 #############
+# Easily reload zsh config
+alias re-source="source $ZDOTDIR/zshrc"
 
 ## Run one-off command in current directory, in specified image
 ## See: https://github.com/michaloo/drun
 alias drun='docker run -u $(id -u):$(id -g) -v ~/.drun:/drun -e HOME=/drun -v `pwd`:/app --workdir /app -it --rm --network=host'
+
+alias dev="yarn run dev"
+alias build="yarn run build"
+
--- config/zsh/commands.zsh
+++ config/zsh/commands.zsh
@@ -45,7 +45,35 @@ fi
 
 ######################################################################
 #### FASD
+
 if (( $+commands[fasd] )); then
   eval "$(fasd --init auto)"
   alias j=z
+fi
+
+######################################################################
+#### FD
+#### https://github.com/sharkdp/fd
+
+if (( $+commands[fd] )); then
+  alias find="fd"
+fi
+
+######################################################################
+#### EXA
+#### https://github.com/ogham/exa
+
+if (( $+commands[exa] )); then
+  alias l='exa --all --header --long --color-scale'
+  alias lg='exa --all --grid --header --long --color-scale'
+  alias lt='exa --all --header --long --tree --color-scale --ignore-glob .git'
+  function lti() {
+    emulate -LR zsh
+    exa --all --header --long --tree --color-scale --ignore-glob ".git|$1" ${@:2}
+  }
+  alias ltl='exa --all --header --long --tree --color-scale --ignore-glob .git --level'
+  function ltli() {
+    emulate -LR zsh
+    exa --all --header --long --tree --color-scale --level $1 --ignore-glob ".git|$2" ${@:3}
+  }
 fi
